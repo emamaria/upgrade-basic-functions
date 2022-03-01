0f15314182eaf685452f5f50fc738cc0c7f98803
emamaria <emascript@gmail.com>
2022-03-01 21:16:48 +0100

    chore: add iteracion3

---
+++ iteracion3.js
@@ -0,0 +1,20 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+
+function sumNumbers(list){
+ let sum = 0;
+
+ for(let i = 0; i < list.length; i++ ){
+
+    sum += list[i]
+ }
+
+ return sum
+
+
+}
+
+console.log(sumNumbers(numbers));
+
+
+
